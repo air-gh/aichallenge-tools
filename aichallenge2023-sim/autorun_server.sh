6476eb1785fc34af58da0fe3f0b91d3599bbfdb5
seigot <s.takada.3o3@gmail.com>
2023-08-23 09:44:43 +0900

    update autorun_server.sh to retry AWSIM when mem error

--- aichallenge2023-sim/autorun_server.sh
+++ aichallenge2023-sim/autorun_server.sh
@@ -55,12 +55,23 @@ function run_awsim(){
     sleep 5
     echo "-- run AWSIM... -->"
     echo "CMD: ${AWSIM_EXEC_COMMAND}"
-    gnome-terminal -- bash -c "${AWSIM_EXEC_COMMAND}" &
-    sleep 15
+    #gnome-terminal -- bash -c "${AWSIM_EXEC_COMMAND}" &
+    #sleep 15
+    for ((i=0; i<20; i++));
+    do
+	gnome-terminal -- bash -c "${AWSIM_EXEC_COMMAND}" &
+	sleep 15
+	PROCESS_CNT=`ps -aux | grep "docker run --rm -it --name ${AWSIM_ROCKER_NAME}" | wc -l`
+	if [ ${PROCESS_CNT} -ge 2 ]; then
+	    break
+	fi
+	echo "no process ${AUTOWARE_ROCKER_NAME}, retry.."
+    done
 
     AWSIM_WID=`xdotool search --name "AWSIM"`
     xdotool windowmove ${AWSIM_WID} 0 200
     xdotool windowsize ${AWSIM_WID} 750 1100
+
     return
 }
 
